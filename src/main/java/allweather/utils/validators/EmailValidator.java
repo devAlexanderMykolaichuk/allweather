6295baa4a161722ccadc0e78720cebf2396778f1
mykolaichuk <mykolaichukalexander@gmail.com>
2019-10-07 16:08:28 +0300

    fix validator

--- src/main/java/allweather/utils/validators/EmailValidator.java
+++ src/main/java/allweather/utils/validators/EmailValidator.java
@@ -11,7 +11,7 @@ public class EmailValidator implements ConstraintValidator<EmailConstraint, Stri
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return value != null && value.matches("[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}");
+        return value != null && value.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}");
     }
 
 
